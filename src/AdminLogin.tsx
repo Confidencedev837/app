ed78a3029b9e4bb7474e459ae07ce92e49e2e9be
Confidencedev837 <confidenceorok30@gmail.com>
2025-04-03 16:23:06 +0100

    bb

--- src/AdminLogin.tsx
+++ src/AdminLogin.tsx
@@ -11,7 +11,7 @@ const AdminLogin: React.FC = () => {
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
     // In production use secure authentication. Here, credentials are hardcoded.
-    if (username === 'admin' && password === 'password') {
+    if (username === 'admin' && password === 'p') {
       login();
       navigate('/admin/dashboard');
     } else {
